509c90f929c2a1809340872cc76c19c9fd1ccc39
120m4n <romansarmientogomez@gmail.com>
2023-11-11 19:57:59 +0000

    se agrega crud sobre router posts

--- database/postgres.go
+++ database/postgres.go
@@ -24,6 +24,13 @@ func (p *PostgresRepository) InsertUser(ctx context.Context, user *models.User) 
 	return err
 }
 
+
+func (p *PostgresRepository) InsertPost(ctx context.Context, post *models.Post) error {
+	_, err := p.db.ExecContext(ctx, "INSERT INTO posts (id, post_content, user_id) VALUES ($1, $2, $3)",
+	post.ID, post.PostContent, post.UserId)
+	return err
+}
+
 func (p *PostgresRepository) GetUserById(ctx context.Context, id int64) (*models.User, error) {
 	row := p.db.QueryRowContext(ctx, "SELECT id, email FROM users WHERE id = $1", id)
 	var user models.User
@@ -32,6 +39,16 @@ func (p *PostgresRepository) GetUserById(ctx context.Context, id int64) (*models
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (p *PostgresRepository) GetPostById(ctx context.Context, id string) (*models.Post, error) {
+	row := p.db.QueryRowContext(ctx, "SELECT id, post_content, user_id, created_at FROM posts WHERE id = $1", id)
+	var post models.Post
+	err := row.Scan(&post.ID, &post.PostContent, &post.UserId, &post.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &post, nil
 }
 
 func (p *PostgresRepository) GetUserByEmail(ctx context.Context, email string) (*models.User, error) {
@@ -43,6 +60,18 @@ func (p *PostgresRepository) GetUserByEmail(ctx context.Context, email string) (
 	}
 	return &user, nil
 }
+
+func (p *PostgresRepository) UpdatePost(ctx context.Context, post *models.Post) error {
+	_, err := p.db.ExecContext(ctx, "UPDATE posts SET post_content = $1 WHERE id = $2 and user_id = $3", post.PostContent, post.ID, post.UserId)
+	return err
+}
+
+func (p *PostgresRepository) DeletePost(ctx context.Context, id string, userid int64) error {
+	_, err := p.db.ExecContext(ctx, "DELETE FROM posts WHERE id = $1 and user_id = $2", id, userid)
+	return err
+}
+
+
 
 func (p *PostgresRepository) Close() error {
 	return p.db.Close()
---
+++ handlers/post.go
@@ -0,0 +1,253 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"rest-ws/models"
+	"rest-ws/repository"
+	"rest-ws/server"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/gorilla/mux"
+	"github.com/segmentio/ksuid"
+)
+
+type UpsertPostRequest struct {
+	PostContent string `json:"post_content"`
+}
+
+type PostResponse struct {
+	Message string `json:"message"`
+	Status  bool   `json:"status"`
+	Id 	string  `json:"id"`
+	PostContent string `json:"post_content"`
+}
+
+type PostUpdateResponse struct {
+	Message string `json:"message"`
+	Status  bool   `json:"status"`
+}
+
+
+
+func InsertPostHandler(s server.Server) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		value := r.Context().Value("userToken")
+		if value == nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token, ok := value.(*jwt.Token)
+		if !ok {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if claims, ok := token.Claims.(*models.AppClaims); ok {
+			userID := claims.UserId
+			var req UpsertPostRequest
+			err := json.NewDecoder(r.Body).Decode(&req)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			// Check if PostContent is empty
+            if req.PostContent == "" {
+                http.Error(w, "Post content cannot be empty", http.StatusBadRequest)
+                return
+            }
+
+			id, err := ksuid.NewRandom()
+			if err != nil{
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			
+			post := models.Post{
+				ID: id.String(),
+				PostContent: req.PostContent,
+				UserId: userID,
+			}
+
+			err = repository.InsertPost(r.Context(), &post)
+			if err != nil{
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(PostResponse{
+				Message: "Post created successfully",
+				Status:  true,
+				Id: post.ID,
+				PostContent: post.PostContent,
+			})
+		} else {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+	}
+}
+
+func GetPostByIdHandler (s server.Server) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		value := r.Context().Value("userToken")
+		if value == nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token, ok := value.(*jwt.Token)
+		if !ok {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if claims, ok := token.Claims.(*models.AppClaims); ok {
+			userID := claims.UserId
+			vars := mux.Vars(r)
+			postID := vars["id"]
+
+			post, err := repository.GetPostById(r.Context(), postID)
+			if err != nil{
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			if post.UserId != userID{
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(PostResponse{
+				Message: "Post retrieved successfully",
+				Status:  true,
+				Id: post.ID,
+				PostContent: post.PostContent,
+			})
+		} else {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+	}
+}
+
+func UpdatePostHandler(s server.Server) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		value := r.Context().Value("userToken")
+		if value == nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token, ok := value.(*jwt.Token)
+		if !ok {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if claims, ok := token.Claims.(*models.AppClaims); ok {
+			userID := claims.UserId
+			vars := mux.Vars(r)
+			postID := vars["id"]
+
+			post, err := repository.GetPostById(r.Context(), postID)
+			if err != nil{
+				// http.Error(w, err.Error(), http.StatusInternalServerError)
+				http.Error(w, "Post not found", http.StatusNotFound)
+				return
+			}
+
+			if post.UserId != userID{
+				http.Error(w, "Unauthorized over not user post", http.StatusUnauthorized)
+				return
+			}
+
+			var req UpsertPostRequest
+			err = json.NewDecoder(r.Body).Decode(&req)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			// Check if PostContent is empty
+			if req.PostContent == "" {
+				http.Error(w, "Post content cannot be empty", http.StatusBadRequest)
+				return
+			}
+
+			post.PostContent = req.PostContent
+
+			err = repository.UpdatePost(r.Context(), post)
+			if err != nil{
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(PostUpdateResponse{
+				Message: "Post updated successfully",
+				Status:  true,
+			})
+		} else {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+	}
+}
+
+func DeletePostHandler(s server.Server) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		value := r.Context().Value("userToken")
+		if value == nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token, ok := value.(*jwt.Token)
+		if !ok {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if claims, ok := token.Claims.(*models.AppClaims); ok {
+			userID := claims.UserId
+			vars := mux.Vars(r)
+			postID := vars["id"]
+
+			post, err := repository.GetPostById(r.Context(), postID)
+			if err != nil{
+				// http.Error(w, err.Error(), http.StatusInternalServerError)
+				http.Error(w, "Post not found", http.StatusNotFound)
+				return
+			}
+
+			if post.UserId != userID{
+				http.Error(w, "Unauthorized over not user post", http.StatusUnauthorized)
+				return
+			}
+
+			err = repository.DeletePost(r.Context(), postID, userID)
+			if err != nil{
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(PostUpdateResponse{
+				Message: "Post deleted successfully",
+				Status:  true,
+			})
+		} else {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+	}
+}
